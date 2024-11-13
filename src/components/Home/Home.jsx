c7553addec44a845950876cde690fdb2deece015
Stefan Bloom <stefanjbloom88@gmail.com>
2024-11-13 14:10:03 -0700

    feat: images created and bound to subscription id's

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,8 @@
 import './Home.css'
 import teapotIcon from '../../data/icons/teapot.png'
+import mintImage from '../../data/images/mint_tea.jpeg';
+import matchaImage from '../../data/images/matcha.jpeg';
+import tulsiImage from '../../data/images/tulsi.jpeg';
 import SearchBar from '../SearchBar/SearchBar'
 import { useNavigate } from 'react-router-dom'
 
@@ -7,23 +10,40 @@ const Home = ({ subscriptions }) => {
 
   const navigate = useNavigate();
   console.log("Subscriptions array:", subscriptions);
+
+  const imagesToIds = {
+    2: matchaImage,
+    3: tulsiImage,
+    4: mintImage
+  }
+
   return(
     <main className='home-main'>
       <SearchBar/>
       <div className='home-div'>
-        <h2 className='home-header'>A Plea for Tea Subscription Service</h2>
+        <h1 className='home-header'>A Plea for Tea Subscription Service</h1>
           <img className='tea-icon' alt='Teapot Icon' src={teapotIcon}></img>
         <div className='subscription-list'>
-          {subscriptions.map((subscription, index) => (
+          {subscriptions.map((subscription) => {
+            const subscriptionImage = imagesToIds[subscription.id];
+           return (
             <div 
               className='subscription-index'
-              key={index}
+              key={subscription.id}
               onClick={()=> navigate(`/subscriptions/${subscription.id}`)}
             >
               <h3 className='subscription-title'>{subscription.title}</h3>
-              <h4 className='subscription-price'>{subscription.price}/{subscription.frequency}</h4>
+              <h4 className='subscription-price'>{subscription.price}/{subscription.frequency}
+              </h4>
+              {subscriptionImage && (
+                <img
+                  className='subscription-image'
+                  src={subscriptionImage}
+                  alt="Tea Image"
+                />
+              )}
             </div>
-          ))}
+          )})}
         </div>
       </div>
     </main>

e3e5c98d59d48cac4d7e59ab318c32dfbeb0c148
Stefan Bloom <stefanjbloom88@gmail.com>
2024-11-13 16:21:55 -0700

    componenet is fetching and rendering

--- src/components/SubscriptionDetails/SubscriptionDetails.jsx
+++ src/components/SubscriptionDetails/SubscriptionDetails.jsx
@@ -1,11 +1,34 @@
 import './SubscriptionDetails.css'
-import SearchBar from '../SearchBar/SearchBar'
-import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const SubscriptionDetails = () => {
 
+  const { id } = useParams();
+  const [subscription, setSubscription] = useState(null);
 
+  useEffect(() => {
+    fetchASubscription(id);
+  }, [id]);
 
+  function fetchASubscription(subscriptionId) {
+    fetch(`http://localhost:3000/api/v1/subscriptions/${subscriptionId}`)
+      .then(response => response.json())
+      .then(data => setSubscription(data.data.attributes))
+      .catch(error => console.error("Error :", error))
+  };
+
+  if (!subscription) return <p>Loading subscription details...</p>;
+
+  return (
+    <div className='subscription-overlay'>
+      <h2 className='subscription-title'>{subscription.title}</h2>
+      <ul className='subscription-list'>
+        <li className='subscription-price'>Price: {subscription.price}</li>
+        <li className='subscription-status'>Status: {subscription.status}</li>
+      </ul>
+    </div>
+  )
 
 };
 export default SubscriptionDetails;
